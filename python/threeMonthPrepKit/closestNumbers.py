d30bb9e10d58b66c78ed0616c7af005013825bdb
Jason Ruffin <jason.don.ruffin@gmail.com>
2025-03-24 11:15:12 -0700

    use ssh key

---
+++ python/threeMonthPrepKit/closestNumbers.py
@@ -0,0 +1,31 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'closestNumbers' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def closestNumbers(arr):
+    # Write your code here
+
+if __name__ == '__main__':
+    fptr = sys.stdout
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = closestNumbers(arr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
